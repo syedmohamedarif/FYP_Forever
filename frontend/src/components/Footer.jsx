c42e655ddad95bf3e6bbabe271e8ac08b5a41bae
syedmohamedarif <158511819+syedmohamedarif@users.noreply.github.com>
2024-11-03 23:58:56 +0530

    Update Footer.jsx

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -9,7 +9,7 @@ const Footer = () => {
         <div>
             <img src={assets.logo} className='mb-5 w-32' alt="" />
             <p className='w-full md:w-2/3 text-gray-600'>
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+            Your ultimate destination for timeless choices and lasting quality. At Forever, we believe in creating shopping experiences that go beyond trends and fleeting fads. Every product we offer is selected to bring enduring value, combining style, reliability, and purpose for those who seek more than just another purchase. Whether you're refreshing your wardrobe, upgrading your home, or looking for unique gifts, Forever ensures each choice you make is meaningful and built to last. Discover products designed for your life today and cherished for the years to come—because at Forever, every choice truly lasts a lifetime.
             </p>
         </div>
 
@@ -26,8 +26,8 @@ const Footer = () => {
         <div>
             <p className='text-xl font-medium mb-5'>GET IN TOUCH</p>
             <ul className='flex flex-col gap-1 text-gray-600'>
-                <li>+1-212-456-7890</li>
-                <li>contact@foreveryou.com</li>
+                <li>+91 8667037069</li>
+                <li>syedmohamedarif2208@gmail.com</li>
             </ul>
         </div>
 
@@ -35,7 +35,7 @@ const Footer = () => {
 
         <div>
             <hr />
-            <p className='py-5 text-sm text-center'>Copyright 2024@ forever.com - All Right Reserved.</p>
+            <p className='py-5 text-sm text-center'>Copyright 2024@ Syed Mohamed Arif S - All Right Reserved.</p>
         </div>
 
     </div>
